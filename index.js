1ed3854b9ccf77fdd4a9a3de121b758336f17e06
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-09-15 12:43:59 -0700

    commit tree a2ab37d1afd0ec5e1efdc011b31aacffac526982

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your functions here
+// const mapNegative = function(input) {
+// 	return input * -1
+// }
+
+function map(array, func) {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    result.push(func(array[i]));
+  }
+  return result;
+}
+
+function reduce(array, callback, start) {
+  let memo;
+  // start ? (memo = start) : (memo = 0);
+
+  if (start) {
+    memo = start;
+    for (let i = 0; i < array.length; i++) {
+      memo = callback(array[i], memo);
+    }
+  } else {
+    memo = array[0];
+    for (let i = 1; i < array.length; i++) {
+      memo = callback(array[i], memo);
+    }
+  }
+  return memo;
+}
